34e6542e83c7c8997f3779e00a733de4bc03a7a5
leonardo.tavares <leonardo.tavares@NT-04539>
2022-02-22 16:50:42 -0300

    Services e repositorio (estrutura)

---
+++ AdegaAmbev/Produtos/Service/ProdutoService.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using AdegaAmbev.Produtos.Entidades;
+
+namespace AdegaAmbev.Produtos.Service {
+    public class ProdutoService
+    {
+        public void CadastrarProduto(){
+
+        }
+
+        public void AtualizarProduto(){
+            
+        }
+
+        public List<Produto> BuscarTodosOsProdutos(){
+            
+        }
+
+        public List<Produto> ListarProdutosPorFiltros(string? nome, TipoBebida? tipoBebida){
+            
+        }
+    }
+}
