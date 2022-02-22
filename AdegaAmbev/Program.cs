f2cdbcafffd4e977b56e5fcc061ec8d3a49b3512
ivo.junior <ivo.junior@ambevtech.com.br>
2022-02-22 14:56:57 -0300

    inicia modelo base do projeto

---
+++ AdegaAmbev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AdegaAmbev
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Startup();
+        }
+
+        public static void Startup()
+        {
+            // chamar intefaces aqui para inicializar.
+        }
+    }
+}
