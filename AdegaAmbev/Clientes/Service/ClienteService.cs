edda3ebcc7d78bfeea8fbdcc4197cadd8b2acede
Kauê Felipe Salvio <kaue.salvio@ambevtech.com.br>
2022-02-25 15:36:14 -0300

    atualização cliente

--- AdegaAmbev/Clientes/Service/ClienteService.cs
+++ AdegaAmbev/Clientes/Service/ClienteService.cs
@@ -4,6 +4,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace AdegaAmbev.Clientes.Service
 {
@@ -46,6 +47,39 @@ namespace AdegaAmbev.Clientes.Service
         
         public Cliente FiltrarClientePorEmail(Cliente[] clientes, string email) {
             return (Cliente)clientes.Where(cliente => cliente.Email == email);
+        }
+
+        public void AtualizarCliente(string jsonFile)
+        {
+            string json = File.ReadAllText(jsonFile);
+
+            var jObject = JObject.Parse(json);
+            JArray clientes = (JArray)jObject["clientes"];
+            Console.Write("Digite o id do cliente");
+            var clienteId = Convert.ToInt32(Console.ReadLine());
+
+            if (clienteId > 0)
+            {
+                Console.Write("Digite o novo nome do Cliente: ");
+                var nomeCliente = Convert.ToString(Console.ReadLine());
+
+                Console.Write("Digite o novo email do Cliente: ");
+                var emailCliente = Convert.ToString(Console.ReadLine());
+
+                foreach (var cliente in clientes.Where(obj => obj["Id"].Value<int>() == clienteId))
+                {
+                    cliente["nomeCliente"] = !string.IsNullOrEmpty(nomeCliente) ? nomeCliente : "";
+                    cliente["emailCliente"] = !string.IsNullOrEmpty(emailCliente) ? emailCliente : "";
+                }
+
+                jObject["clientes"] = clientes;
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jObject, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(jsonFile, output);
+            }
+            else
+            {
+                Console.Write("Id inválido");
+            }
         }
     }
 }
