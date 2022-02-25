e8752023e67d574975d944c9d13210a1674be51f
ivo.junior <ivo.junior@ambevtech.com.br>
2022-02-25 16:09:05 -0300

    ajustes finais Produto

--- AdegaAmbev/Produtos/Service/ProdutoService.cs
+++ AdegaAmbev/Produtos/Service/ProdutoService.cs
@@ -1,16 +1,88 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using AdegaAmbev.Produtos.Entidades;
 
-namespace AdegaAmbev.Produtos.Service {
+namespace AdegaAmbev.Produtos.Service
+{
     public class ProdutoService
     {
-        public void CadastrarProduto(){
+        private string Host { get; set; }
+        private TipoBebidaService bebidaService { get; set; }
 
+        public ProdutoService()
+        {
+            bebidaService = new TipoBebidaService();
+            Host = Directory.GetCurrentDirectory() + @"..\..\..\..\Banco\Produto.json";
         }
 
-        public void AtualizarProduto(){
-            
+        public bool CadastrarProduto(Produto produto)
+        {
+            using FileStream stream = File.OpenRead(Host);
+            if (bebidaService.ExisteTipoBebida(produto.TipoBebida))
+                return false;
+
+            var produtosDB = JsonSerializer.DeserializeAsync<List<Produto>>(stream).Result;
+            stream.Close();
+            produto.Id = produtosDB.Count + 1;
+            produtosDB.Add(produto);
+            stream.Close();
+
+            File.WriteAllText(Host, JsonSerializer.Serialize(produtosDB));
+            return true;
+        }
+
+        public bool AtualizarProduto(int id, Produto produto)
+        {
+            using FileStream stream = File.OpenRead(Host);
+            if (bebidaService.ExisteTipoBebida(produto.TipoBebida))
+                return false;
+
+            var produtosDb = JsonSerializer.DeserializeAsync<List<Produto>>(stream).Result;
+            stream.Close();
+            var produtoDB = produtosDb.First(x => x.Id == id);
+            var index = produtosDb.FindIndex(x => x.Id == id);
+            produto.Id = id;
+            produtosDb[index] = produto;
+
+            File.WriteAllText(Host, JsonSerializer.Serialize(produtosDb));
+            return true;
+        }
+
+        public List<Produto> BuscarTodosOsProdutos()
+        {
+            using FileStream stream = File.OpenRead(Host);
+            return JsonSerializer.DeserializeAsync<List<Produto>>(stream).Result;
+        }
+
+        public Produto GetId(int id)
+        {
+            using FileStream stream = File.OpenRead(Host);
+            var produtosDb = JsonSerializer.DeserializeAsync<List<Produto>>(stream).Result;
+            stream.Close();
+            return produtosDb.First(x => x.Id == id);
+        }
+
+        public List<Produto> BuscarProdutosPorFiltros(string nome = "", string tipoBebida = "")
+        {
+            using FileStream stream = File.OpenRead(Host);
+            var produtosDB = JsonSerializer.DeserializeAsync<List<Produto>>(stream).Result;
+            stream.Close();
+
+            if (!string.IsNullOrWhiteSpace(nome) && !string.IsNullOrWhiteSpace(tipoBebida))
+            {
+                return produtosDB.Where(x => x.Nome == nome && x.TipoBebida == tipoBebida).ToList();
+            }
+            else if (!string.IsNullOrWhiteSpace(nome))
+            {
+                return produtosDB.Where(x => x.Nome == nome).ToList();
+            }
+            else
+            {
+                return produtosDB.Where(x => x.TipoBebida == tipoBebida).ToList();
+            }
         }
     }
 }
