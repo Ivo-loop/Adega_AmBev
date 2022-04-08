0b16f66f7292c958b0cdb84483428b9d5a9c3a16
ivo.junior <ivo.junior@ambevtech.com.br>
2022-04-08 14:46:47 -0300

    add projeto test

---
+++ ClienteTestRoots/Program.cs
@@ -0,0 +1,31 @@
+﻿using AdegaAmbev.Clientes.Entidades;
+using AdegaAmbev.Clientes.Service;
+using System;
+
+namespace ClienteTestRoots
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void testValidarDadosClientes()
+        {
+            ClienteService clienteService = new ClienteService();
+            Cliente cliente = new Cliente()
+            {
+                Nome = "josue",
+                Email = "josue.santos@gmail.com.br"
+            };
+
+            var valor = clienteService.ValidarCliente(cliente);
+
+            if (valor != "sucesso")
+                Console.WriteLine("test de validar user falhando");
+            else
+                Console.WriteLine("deu sucesso");
+        }
+    }
+}
