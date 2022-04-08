4e083e2a2d60d7957567edba7a7144f80df2f311
ivo.junior <ivo.junior@ambevtech.com.br>
2022-04-08 16:13:47 -0300

    add banco utils

---
+++ TestRoots/Common/BancoUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace TestRoots.Common
+{
+    public class BancoUtils
+    {
+        private string Path { get; set; }
+        public BancoUtils(string path)
+        {
+            Path = path;
+            this.ClearBanco();
+        }
+
+        public void ClearBanco()
+        {
+            File.WriteAllText(Path, "");
+        }
+    }
+}
