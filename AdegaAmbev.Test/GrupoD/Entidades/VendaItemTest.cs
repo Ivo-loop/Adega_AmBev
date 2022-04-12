c46e00d2c8b67a81dbdb8ad3d84587443d71b6ab
Julia Kruger <julia.kruger@ambevtech.com.br>
2022-04-12 16:27:58 -0300

    Add testes entidade VendaItem - feitos pela Debora

---
+++ AdegaAmbev.Test/GrupoD/Entidades/VendaItemTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace AdegaAmbev.Test.GrupoD.Entidades
+{
+    public class VendaItemTest
+    {
+        [TestCase(123, 10)]
+        [TestCase(542, 33)]
+        [TestCase(12, 123)]
+        public void Instanciar_OProdutoParaVenda(int codigoProduto, int quantidadeInicial)
+        {
+            // Arrange
+
+            // Act
+            var vendaItem = new Estoque.Entidades.VendaItem(codigoProduto, quantidadeInicial);
+
+            // Assert
+            Assert.AreEqual(quantidadeInicial, vendaItem.Quantidade);
+            Assert.AreEqual(codigoProduto, vendaItem.ProdutoId);
+        }
+
+        [TestCase(123, 10)]
+        [TestCase(542, 33)]
+        [TestCase(12, 123)]
+        public void TranportarDadosParaStrings(int codigoProduto, int quantidadeInicial)
+        {
+            // Arrange
+            var vendaItem = new Estoque.Entidades.VendaItem(codigoProduto, quantidadeInicial);
+
+            // Act
+            var resultado = vendaItem.ToString();
+
+            // Assert
+            Assert.AreEqual(@$"Produto Id = {vendaItem.ProdutoId}  Quantidade = {vendaItem.Quantidade}", resultado);
+        }
+    }
+}
